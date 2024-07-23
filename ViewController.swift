081c6703a5368fc1ae8f55ff97ad081fcc340ae0
시모니(Simoni) <145527459+simoni-git@users.noreply.github.com>
2024-07-23 17:54:51 +0900

    Start
    비동기처리가 하나도 없는 상태 => 앱 실행시 렉이 엄청남.

--- ViewController.swift
+++ ViewController.swift
@@ -10,12 +10,43 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    var urls: [URL] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
-       
+        loadPlist()
+        print("살라살라")
+    }
+    
+    func loadPlist() {
+        guard let url = Bundle.main.url(forResource: "Photos", withExtension: "plist"),
+              //⬆️번들에서 "Photos.plist" 파일의 URL을 가져옵니다.
+              //url 변수는 이 파일의 URL을 가리킵니다. 만약 파일을 찾지 못하면 nil이 됩니다.
+              let contents = try? Data(contentsOf: url),
+              //⬆️파일의 내용을 Data 객체로 읽어옵니다.
+              //try?를 사용하여 오류가 발생할 경우 nil을 반환합니다.
+              let serial = try? PropertyListSerialization.propertyList(from: contents, format: nil),
+              //⬆️plist 데이터를 Swift 객체로 변환합니다.
+              //contents는 Data 객체로 변환된 plist 파일의 내용입니다.
+              //format은 출력 형식 정보를 담기 위한 파라미터로 nil을 사용하여 무시할 수 있습니다.
+              //serial 변수는 plist 파일의 내용을 직렬화한 결과 객체를 가리킵니다. 오류가 발생하면 nil이 됩니다.
+              let serialURLs = serial as? [String]
+                //⬆️serial을 [String] 타입으로 캐스팅합니다.
+                //serialUrls 변수는 plist 파일에 저장된 URL 문자열 배열을 가리킵니다. 만약 캐스팅이 실패하면 nil이 됩니다.
+        else {
+          return print("어디선가 사고남")
+        }
+        
+        urls = serialURLs.compactMap({ URL(string: $0)})
+        /* ⬆️
+         compactMap 메서드를 사용하여 serialUrls 배열의 각 요소를 URL 객체로 변환합니다.
+         URL(string: $0): 각 문자열을 URL 객체로 초기화합니다. 유효하지 않은 URL 문자열은 nil로 변환됩니다.
+         compactMap은 nil 값을 제거하고 유효한 URL 객체들만 포함한 새로운 배열을 반환합니다.
+         urls 변수는 유효한 URL 객체들을 가리키게 됩니다.
+         */
+              
     }
 
 
@@ -23,13 +54,21 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return urls.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as? MyCell else {
             return UICollectionViewCell()
         }
+        
+        if let data = try? Data(contentsOf: urls[indexPath.item]),
+           let img = UIImage(data: data) {
+            cell.imgView.image = img
+        } else {
+            cell.imgView.image = nil
+        }
+           
         
         return cell
     }
